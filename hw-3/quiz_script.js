e403486f15470e74113ec80341ef2b786d31a27b
Zeljko28 <zeljkoppvc@gmail.com>
2020-12-22 01:25:52 +0100

    Uradjen pocetak dizajna samog kviza(stranica sa pitanjima i odgovorima)
    Dodate sitne funkcionalnosti , prelazak sa sekcije na sekciju pritiskom na dugme..

--- hw-3/quiz_script.js
+++ hw-3/quiz_script.js
@@ -1,6 +1,17 @@
 var submitBtn = document.getElementById("submit");
 var loginDiv = document.getElementById("login");
 
+var startBtn = document.getElementById("start");
+var rulesDiv = document.getElementById("rules");
+
+var quizDiv = document.getElementById("quiz");
+
 submitBtn.onclick = () => {
     loginDiv.style.display = "none";
+    rulesDiv.style.display = "block";
+}
+
+startBtn.onclick = () => {
+    rulesDiv.style.display = "none";
+    quizDiv.style.display = "block";
 }
