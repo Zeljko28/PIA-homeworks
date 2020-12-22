542713826afc9049910cb7ca69b593122fec3476
Zeljko28 <zeljkoppvc@gmail.com>
2020-12-22 14:48:04 +0100

    Sitne prepravke dizajna strane sa pitanjima i dodat tajmer(bez funkcionalnosti), i dodata validacija za unos imena na prvoj strani

--- hw-3/quiz_script.js
+++ hw-3/quiz_script.js
@@ -1,17 +1,39 @@
-var submitBtn = document.getElementById("submit");
-var loginDiv = document.getElementById("login");
 
-var startBtn = document.getElementById("start");
-var rulesDiv = document.getElementById("rules");
+/* Login Page */
 
-var quizDiv = document.getElementById("quiz");
+var loginBtn = document.getElementById("submit");
+var nameValue;
 
-submitBtn.onclick = () => {
-    loginDiv.style.display = "none";
-    rulesDiv.style.display = "block";
+loginBtn.onclick = () => {
+    var loginDiv = document.getElementById("login");
+    var rulesDiv = document.getElementById("rules");
+    nameValue = document.getElementById("input").value;
+    var text = nameValue.split(" ").join("");
+
+    if(text == ""){
+        document.getElementById("warn").innerHTML = "Niste uneli ime!"
+    }
+    else{
+        loginDiv.style.display = "none";
+        rulesDiv.style.display = "block";
+    }
 }
 
+/* Rules Page */
+
+var startBtn = document.getElementById("start");
+
 startBtn.onclick = () => {
+    var rulesDiv = document.getElementById("rules");
+    var quizDiv = document.getElementById("quiz");
     rulesDiv.style.display = "none";
     quizDiv.style.display = "block";
 }
+
+
+/* Quiz Page */
+
+
+
+
+
